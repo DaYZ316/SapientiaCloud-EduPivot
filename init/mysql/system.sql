0709ecf918f182b43b9ce2a6927191f9bf26ca1e
DaYZ <654011721@qq.com>
2025-07-15 01:50:31 +0800

    role, permission表设计

--- init/mysql/system.sql
+++ init/mysql/system.sql
@@ -1,19 +1,80 @@
+-- ===============================================================
+-- Table: sys_permission
+-- ===============================================================
+create table sys_permission
+(
+    id              binary(16)                         not null
+        primary key,
+    parent_id       binary(16)                         null,
+    permission_name varchar(50)                        not null,
+    permission_key  varchar(100)                       not null,
+    sort            int      default 0                 null,
+    status          int      default 0                 null,
+    create_time     datetime default CURRENT_TIMESTAMP null,
+    update_time     datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    is_deleted      int      default 0                 null
+);
+
+-- ===============================================================
+-- Table: sys_role
+-- ===============================================================
+create table sys_role
+(
+    id          binary(16)                         not null
+        primary key,
+    role_name   varchar(30)                        not null,
+    role_key    varchar(100)                       not null,
+    sort        int      default 0                 null,
+    status      int      default 0                 null,
+    description varchar(500)                       null,
+    create_time datetime default CURRENT_TIMESTAMP null,
+    update_time datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    is_deleted  int      default 0                 null
+);
+
+-- ===============================================================
+-- Table: sys_user
+-- ===============================================================
 create table sys_user
 (
-    id              binary(16)                         not null comment '用户ID'
+    id              binary(16)                         not null
         primary key,
-    username        varchar(255)                       null comment '用户名',
-    password        varchar(255)                       null comment '密码',
-    nick_name       varchar(255)                       null comment '用户昵称',
-    email           varchar(255)                       null comment '邮箱',
-    mobile          varchar(255)                       null comment '手机号',
-    gender          int                                null comment '性别 (0=未知, 1=男, 2=女)',
-    avatar          varchar(255)                       null comment '用户头像URL',
-    status          int      default 0                 null comment '状态 (0=正常, 1=停用)',
-    last_login_time datetime                           null comment '最后登录时间',
-    create_time     datetime default CURRENT_TIMESTAMP null comment '创建时间 (系统自动生成)',
-    update_time     datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间 (系统自动生成)',
-    is_deleted      int      default 0                 null comment '逻辑删除标记 (内部使用)'
-)
-    comment '系统用户表';
+    username        varchar(255)                       null,
+    password        varchar(255)                       null,
+    nick_name       varchar(255)                       null,
+    email           varchar(255)                       null,
+    mobile          varchar(255)                       null,
+    gender          int                                null,
+    avatar          varchar(255)                       null,
+    status          int      default 0                 null,
+    last_login_time datetime                           null,
+    create_time     datetime default CURRENT_TIMESTAMP null,
+    update_time     datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    is_deleted      int      default 0                 null
+);
 
+-- ===============================================================
+-- Table: sys_role_permission
+-- ===============================================================
+create table sys_role_permission
+(
+    role_id       binary(16) not null,
+    permission_id binary(16) not null,
+    primary key (role_id, permission_id)
+);
+
+create index idx_permission_id_on_role_permission
+    on sys_role_permission (permission_id);
+
+-- ===============================================================
+-- Table: sys_user_role
+-- ===============================================================
+create table sys_user_role
+(
+    user_id binary(16) not null,
+    role_id binary(16) not null,
+    primary key (user_id, role_id)
+);
+
+create index idx_role_id_on_user_role
+    on sys_user_role (role_id);
